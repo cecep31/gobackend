c45490c0e2c71e20b5af8ca5cf08f448c2a013ce
cecep <cecepjanuardi31@gmail.com>
2022-11-28 13:34:33 +0700

    email field

--- pkg/entities/users.go
+++ pkg/entities/users.go
@@ -5,6 +5,7 @@ import "gobackend/database"
 type User struct {
 	database.DefaultModel
 	Username string `json:"username" gorm:"uniqueIndex"`
+	Email    string `json:"email" gorm:"uniqueIndex"`
 	Role     string `json:"role"`
 	Password string `json:"password" gorm:"type:text"`
 	Image    string `json:"image" gorm:"type:text"`
