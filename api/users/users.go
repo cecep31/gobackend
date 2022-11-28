2b66a21a921355dc96aaca5f66eaeea97f478708
cecep <cecepjanuardi31@gmail.com>
2022-11-28 13:40:18 +0700

    fix entitie

--- api/users/users.go
+++ api/users/users.go
@@ -24,6 +24,7 @@ type User struct {
 type Users struct {
 	database.DefaultModel
 	Username string `json:"username"`
+	Email    string `json:"email"`
 	Role     string `json:"role"`
 	Image    string `json:"image" gorm:"type:text"`
 }
