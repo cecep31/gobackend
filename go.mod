edfb11922d532accbaa17b064155cb9425b55ff0
cecep <cecepjanuardi31@gmail.com>
2022-12-19 16:23:02 +0700

    book desc

--- go.mod
+++ go.mod
@@ -10,7 +10,7 @@ require (
 	github.com/golang-jwt/jwt/v4 v4.4.2
 	github.com/joho/godotenv v1.4.0
 	github.com/stretchr/testify v1.8.1
-	golang.org/x/crypto v0.3.0
+	golang.org/x/crypto v0.4.0
 	gorm.io/driver/postgres v1.4.5
 	gorm.io/gorm v1.24.2
 
--- pkg/entities/books.go
+++ pkg/entities/books.go
@@ -1,11 +1,15 @@
 package entities
 
-import "gobackend/database"
+import (
+	"gobackend/database"
+)
 
 type Book struct {
 	database.DefaultModel
 	Title  string `json:"title"`
+	Desc   string `json:"desc" gorm:"type:text"`
 	Author string `json:"author"`
 	Rating int    `json:"rating"`
 	Price  int32  `json:"price" gorm:"default:0"`
+	Image  string `json:"image" gorm:"type:text"`
 }
