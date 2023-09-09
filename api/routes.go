59cf52e84b2e8715760c052a0a74343544d67c21
cecep31 <cecepjanuardi31@gmail.com>
2023-09-09 11:12:04 +0700

    update nothing

--- api/routes.go
+++ api/routes.go
@@ -12,26 +12,29 @@ import (
 )
 
 func UserRouter(app fiber.Router, service user.Service) {
-	app.Post("/users", handlers.AddUser(service))
-	app.Get("/users", handlers.GetUsers(service))
-	app.Get("/users/:id", handlers.GetUser(service))
+	app.Post("users", handlers.AddUser(service))
+	app.Get("users", handlers.GetUsers(service))
+	app.Get("users/:id", handlers.GetUser(service))
+	app.Put("users/:id", func(c *fiber.Ctx) error {
+		return c.SendString("not available")
+	})
 
 }
 
 func AuthRouter(app fiber.Router, service auth.Service) {
-	app.Get("/oauth", handlers.Loginoatuth)
-	app.Get("/oauth/callback", handlers.CallbackHandler(service))
+	app.Get("oauth", handlers.Loginoatuth)
+	app.Get("oauth/callback", handlers.CallbackHandler(service))
 	app.Post("login", handlers.Login)
 	app.Get("profile", middleware.Protected(), handlers.Profile(service))
 }
 
 func PostRouter(app fiber.Router, service posts.Service) {
-	app.Get("/posts", handlers.GetPosts(service))
-	app.Get("/posts/:slug", handlers.GetPost(service))
-	app.Post("/posts", middleware.Protected(), middleware.IsSuperAdmin, handlers.AddPost(service))
-	app.Put("/posts/:id", middleware.Protected(), middleware.IsSuperAdmin, handlers.UpdatePost(service))
+	app.Get("posts", handlers.GetPosts(service))
+	app.Get("posts/:slug", handlers.GetPost(service))
+	app.Post("posts", middleware.Protected(), middleware.IsSuperAdmin, handlers.AddPost(service))
+	app.Put("posts/:id", middleware.Protected(), middleware.IsSuperAdmin, handlers.UpdatePost(service))
 }
 
 func TaskRouter(app fiber.Router, service tasks.Service) {
-	app.Get("/tasks", handlers.GetTasks(service))
+	app.Get("tasks", handlers.GetTasks(service))
 }
